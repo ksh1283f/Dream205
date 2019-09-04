06989d36d350bba00018b5b8756a7d48b2017a14
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-05 01:59:59 +0900

    190905
    헤어트리거를 누르면 굵은 레이저가 나오고 큐브와 플레인에 맞으면 빨간색으로 변합니다 스크립트는 SteamVR>Extras폴더의 LaserPointer(이름맞나?..)입니다

---
+++ New Unity Project (7)/Assets/Lser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lser : MonoBehaviour
+{
+    RaycastHit hit;
+    float MaxDistance = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true)
+        {
+
+        }
+    }
+}
