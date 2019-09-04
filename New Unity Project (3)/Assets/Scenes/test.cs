ac2016dbe9019e6f2f62312ac83281ff2d655e71
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-05 01:55:43 +0900

    190905
    0키를 누르면 사운드가 재생되고, 사운드 재생이 끝나면 다음씬으로 넘어갑니다

---
+++ New Unity Project (3)/Assets/Scenes/test.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // private int levelToLoad;
+
+    
+    // Start is called before the first frame update
+  void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("0"))
+        {
+            audioSource.Play();
+                }
+
+        if (audioSource.isPlaying)
+        {
+            return;
+        }
+
+  else if (audioSource.time>0)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+    public void Delete()
+    {
+        Stop();
+        if (!audioSource.isPlaying)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+/*public void NextLevel()
+    {
+      //levelToLoad = levelIndex;
+        if (!audioSource.isPlaying)
+        {
+       
+        
+            SceneManager.LoadScene(0);
+        }
+    }*/
+
+}
