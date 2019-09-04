f5ae0e6c9b782be7b83eed238fa55245974e21da
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-05 00:47:08 +0900

    190905
    초기 프로젝트파일

---
+++ New Unity Project (4)/Assets/Scripts/SoundTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private GameObject plant;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        plant=GameObject.Find("Potted_Plant");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (plant.transform.localPosition.y>1)
+        {
+            audioSource.Play();
+        }
+
+        if (audioSource.time>0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
