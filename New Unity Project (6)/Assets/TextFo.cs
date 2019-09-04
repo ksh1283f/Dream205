83823c25ee3ea5201942a4ad8ca80010dda17c86
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-05 02:01:48 +0900

    190905
    1키를 누르면 텍스트가 페이드아웃됩니다

---
+++ New Unity Project (6)/Assets/TextFo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFo : MonoBehaviour
+{
+    private Animation TextFadeOut;
+   
+    void Start()
+    {
+        TextFadeOut = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            TextFadeOut.Play();
+        }
+    }
+}
