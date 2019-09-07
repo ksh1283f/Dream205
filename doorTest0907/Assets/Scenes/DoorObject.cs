fa1cd04ded3f52b0a5e6d37acd5e195a40f43af4
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-08 00:18:43 +0900

    190908
    문열기

---
+++ doorTest0907/Assets/Scenes/DoorObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorObject : MonoBehaviour
+{
+    private Animation DoorAni;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoorAni = GetComponent<Animation>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("handle"))
+        {
+            DoorAni.Play();
+        }    
+    }
+
+    void SceneLoading()
+    {
+
+            SceneManager.LoadScene(1);
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+      /*  {
+            if (DoorAni.isPlaying)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }*/
+    }
+}
