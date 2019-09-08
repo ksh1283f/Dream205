f95984437771b1c69312e8cfa149b9821790fa9a
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-09 01:10:00 +0900

    190909
    destroy-오브젝트생성,사운드플레이

---
+++ roomLaserTest0908/Assets/Bug.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : MonoBehaviour
+{
+    private object Cubes;
+
+    public GameObject Sphere;
+    // public AudioSource Sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  var Cube = GameObject.FindWithTag("Interactables");
+      //  var Sound = GameObject.FindWithTag("Sound");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cubes==null)
+        {
+            var Cubes = GameObject.FindWithTag("Interactables");
+            var Sound = GameObject.FindWithTag("Sound");
+            Instantiate(Sphere, Cubes.transform.position, Quaternion.identity);
+            Destroy(Sound);
+        }
+    
+    }
+}
