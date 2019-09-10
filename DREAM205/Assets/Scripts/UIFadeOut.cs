c349b4efa3c985072048117f2381062752b0ae02
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-11 05:03:45 +0900

    190911
    엘베씬작업

---
+++ DREAM205/Assets/Scripts/UIFadeOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFadeOut : MonoBehaviour
+{
+   public Animation StartFo;
+
+
+    void Start()
+    {
+        StartFo = GetComponent<Animation>();
+    }
+
+ 
+    void Update()
+    {
+         if (Input.GetKeyDown("0"))
+          {
+            StartFo.Play();
+          }
+    }
+   
+}
