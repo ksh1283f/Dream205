2749a7e97c768c1b677ed0c741b556c28d2bbcc2
ksh1283f <ksh1283ff@gmail.com>
2019-09-18 23:12:56 +0900

    190918
    웨이포인트 기반 이동 작업

---
+++ DREAM205/Assets/TeleportTest/LaserTest.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class WaypointManager
+//{
+    
+namespace Valve.VR.Extras
+{
+    public class LaserTest : MonoBehaviour
+    {
+        public SteamVR_Behaviour_Pose pose;
+
+        //public SteamVR_Action_Boolean interactWithUI = SteamVR_Input.__actions_default_in_InteractUI;
+        public SteamVR_Action_Boolean interactWithUI = SteamVR_Input.GetBooleanAction("InteractUI");
+        public SteamVR_Action_Boolean Teleport = SteamVR_Input.GetBooleanAction("Teleport");
+
+        public bool active = true;
+        public Color color;
+        public float thickness = 0.002f;
+        public Color clickColor = Color.green;
+        public GameObject holder;
+        public GameObject pointer;
+        public GameObject laserParent;
+        bool isActive = false;
+        public bool addRigidBody = false;
+        public Transform reference;
+        public event PointerEventHandler PointerIn;
+        public event PointerEventHandler PointerOut;
+        public event PointerEventHandler PointerClick;
+        public GameObject gameManager;
+        [SerializeField] Transform playerTrans;
+        [SerializeField] Transform playerCameraTrans;
+
+        Transform previousContact = null;
+        [SerializeField] List<Waypoint> waypoints = new List<Waypoint>();
+        [SerializeField] E_WaypointType presentWaypoint = E_WaypointType.None;
+
+
+        private void Start()
+        {
+            if (pose == null)
+                pose = this.GetComponent<SteamVR_Behaviour_Pose>();
+            if (pose == null)
+                Debug.LogError("No SteamVR_Behaviour_Pose component found on this object");
+
+            if (interactWithUI == null)
+                Debug.LogError("No ui interaction action has been set on this component.");
+
+            laserParent.SetActive(false);
+            ShowWaypoint(presentWaypoint);
+        }
+
+        public virtual void OnPointerIn(PointerEventArgs e)
+        {
+            if (PointerIn != null)
+                PointerIn(this, e);
+        }
+
+        public virtual void OnPointerClick(PointerEventArgs e)
+        {
+            Debug.LogError("oNPointerclick");
+            if (PointerClick != null)
+                PointerClick(this, e);
+        }
+
+        public virtual void OnPointerOut(PointerEventArgs e)
+        {
+            if (PointerOut != null)
+                PointerOut(this, e);
+        }
+
+        void OnDrawGizmos()
+        {
+            float maxDistance = 100;
+            RaycastHit hit;
+            // Physics.Raycast (레이저를 발사할 위치, 발사 방향, 충돌 결과, 최대 거리)
+            Ray raycast = new Ray(transform.position, transform.forward);
+            bool isHit = Physics.Raycast(raycast, out hit, 100, (-1) - (1 << 9));
+
+            Gizmos.color = Color.red;
+            if (isHit)
+            {
+                Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+            }
+            else
+            {
+                Gizmos.DrawRay(transform.position, transform.forward * maxDistance);
+            }
+        }
+
+        private void Update()
+        {
+            if (!isActive)
+            {
+                isActive = true;
+                this.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+            float dist = 100f;
+
+            Ray raycast = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            bool bHit = Physics.Raycast(raycast, out hit, dist, (-1) - (1 << 9));
+            if(hit.transform != null)
+                Debug.LogError("hit tag: " + hit.transform.tag);
+
+
+            
+
+            // 텔레포트
+            if (Teleport != null && Teleport.GetStateDown(pose.inputSource))
+            {
+                Debug.LogError("Teleport");
+                if (hit.transform == null || !hit.transform.CompareTag("WayPoint"))
+                    return;
+
+                float playerYPos = playerTrans.position.y;
+                playerTrans.position = new Vector3(hit.transform.position.x, playerYPos, hit.transform.position.z);
+                presentWaypoint = hit.transform.GetComponent<Waypoint>().WayPointType;
+                ShowWaypoint(presentWaypoint);
+                
+            }
+
+            if (interactWithUI != null && interactWithUI.GetState(pose.inputSource))
+            {
+                Debug.LogError("interactWithUI != null && interactWithUI.GetState(pose.inputSource)");
+                if (bHit && hit.distance < 100f)    // 레이저에 뭔가 닿았고, 물체와 컨트롤러 사이의 거리가 100 미만일 때
+                {
+                    dist = hit.distance;
+                    Debug.LogError("bHit && hit.distance < 100f, dist: " + dist + " hit object: " + hit.transform.gameObject.name);
+                }
+
+                laserParent.transform.localScale = new Vector3(thickness * 15f, thickness * 15f, dist);
+                if (laserParent != null && !laserParent.gameObject.activeSelf)
+                    laserParent.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("else");
+                laserParent.transform.localScale = new Vector3(thickness, thickness, dist);
+                if (laserParent != null && laserParent.gameObject.activeSelf)
+                    laserParent.gameObject.SetActive(false);
+            }
+        }
+
+        void ShowWaypoint(E_WaypointType present)
+        {
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                // 현재위치를 기준으로 앞, 뒤, 제자리만 활성화
+                if (waypoints[i].WayPointType - 1 == present
+                || waypoints[i].WayPointType + 1 == present 
+                || waypoints[i].WayPointType == present)
+                {
+                    waypoints[i].gameObject.SetActive(true);
+                    waypoints[i].ActivateCollider(waypoints[i].WayPointType != present);
+                    continue;
+                }
+
+                waypoints[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ DREAM205/Assets/TeleportTest/Waypoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_WaypointType
+{
+    None,
+    First,
+    Second,
+    Third,
+    Fourth,
+    Fifth,
+    Sixth,
+    Seventh,
+    Eighth,
+    Nineth,
+    Tenth,
+}
+
+public class Waypoint : MonoBehaviour
+{
+    //public Action<E_WaypointType> OnInActive { get; set; }
+    public E_WaypointType WayPointType { get { return wayPointType; } }
+
+    [SerializeField] E_WaypointType wayPointType;
+    Collider col;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    public void ActivateCollider(bool isActive)
+    {
+        if (col == null)
+        {
+            Debug.LogError(wayPointType + " collider is null!");
+            return;
+        }
+
+        col.enabled = isActive;
+    }
+
+}
