a39bc42e095d5b402a3a98bdd76e54576ada6227
ksh1283f <ksh1283ff@gmail.com>
2019-09-21 02:50:57 +0900

    190921
    - 인터랙션 관련 클래스들 추가
    - 싱글톤 클래스 추가

---
+++ DREAM205/Assets/Scripts/Refectoring/LaserInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserInteraction : UserEvent
+{
+    private void Awake()
+    {
+        UserEventType = E_UserEventType.LaserInteraction;
+    }
+
+    protected override void OnTriggerEnter(Collider col)
+    {
+        // 레이저 맞추는 작업
+
+        // 예정된 작업이 모두 끝났을때 호출
+        base.OnTriggerEnter(col);
+    }
+}
---
+++ DREAM205/Assets/Scripts/Refectoring/Singletone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singletone<T> :MonoBehaviour
+    where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if(instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    
+                    instance = obj.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
