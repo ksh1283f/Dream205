bd7f52b058e6325ebff8fb75a0214d03374219a7
yooniju <54897287+yooniju@users.noreply.github.com>
2019-09-24 02:27:15 +0900

    190924 1. 유니티 에셋스토어에서 산 에셋 임포트 2. 씬1에서 문열고 다음씬으로 넘어가는거 씬에 올려보려고 시도했지만 실패..ㅎ

---
+++ DREAM205/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorOpen : MonoBehaviour
+{
+    private Animation DoorAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoorAni = GetComponent<Animation>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name==("Controller (left)"))
+        {
+            DoorAni.Play();
+        }
+    }
+
+    void SceneLoading()
+    {
+
+        SceneManager.LoadScene("level2Room");
+
+    }
+}
