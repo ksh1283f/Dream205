2c62867509bb5b9dda215efb8e356b7cdd78d89b
ksh1283f <ksh1283ff@gmail.com>
2019-09-27 03:52:48 +0900

    190927
    -레벨1씬 전화소리 페이드 효과 추가

--- DREAM205/Assets/Scripts/DoorOpen.cs
+++ DREAM205/Assets/Scripts/DoorOpen.cs
@@ -8,6 +8,8 @@ public class DoorOpen : MonoBehaviour
     public Animation DoorAni;
     public Animation LightOff;
     public AudioSource DoorSound;
+    [SerializeField] AudioSource phoneRing;
+    [SerializeField] float fadeDuration;
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +31,32 @@ public class DoorOpen : MonoBehaviour
         LightOff.Play();
     }
 
-    void SceneLoading()
+    void OnDoorAniEnd()
     {
+        StartCoroutine(FadeSound());
+        //SceneManager.LoadScene("level2Room"); // 로딩은 코루틴 종료 후
+    }
 
-        SceneManager.LoadScene("level2Room");
+    IEnumerator FadeSound()
+    {
+        if (phoneRing == null)
+        {
+            Debug.LogError("ringSound is null");
+            yield break;
+        }
 
+        float volume = phoneRing.volume;
+        float startTime = 0;
+        volume = Mathf.Lerp(0.173f, 0, startTime);
+        while (volume > 0f)
+        {
+            startTime += Time.deltaTime / fadeDuration;
+            volume = Mathf.Lerp(0.173f, 0, startTime);
+            phoneRing.volume = volume;
+
+            yield return null;
+        }
+
+        SceneLoadingManager.Instance.SceneType = E_SceneType.level2Room;
     }
 }
