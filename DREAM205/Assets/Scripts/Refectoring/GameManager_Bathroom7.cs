a38a390abd4af74661ebc319361122390e7415d0
yooniju <54897287+yooniju@users.noreply.github.com>
2019-10-15 22:07:13 +0900

    191015 씬3,5,7 나래이션 배치(0,8은 아직 씬 7 문열리고 씬로딩되는거, 웨이포인트 빼고 완료 씬8 배치, 플레이어핸드 넣음 근데 불켜지고 사운드가 안나옴...ㅜㅡ 사운드 몇가지 교체

---
+++ DREAM205/Assets/Scripts/Refectoring/GameManager_Bathroom7.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Bathroom7 : MonoBehaviour
+{
+    [SerializeField] Animation flickAni;
+
+    void Start()
+    {
+        StartCoroutine(SoundManager.Instance.IntroSoundPlay());
+        SoundManager.Instance.OnSoundPlayEnd += LoadNextScene;
+    }
+
+    // 씬로딩
+    void LoadNextScene()
+    {
+        StartCoroutine(loadNextScene());
+    }
+
+    IEnumerator loadNextScene()
+    {
+        if (flickAni == null)
+        {
+            Debug.LogError("flickAni is null");
+            yield break;
+        }
+
+        flickAni.Play();
+        while (flickAni.isPlaying)
+            yield return null;
+
+        SceneLoadingManager.Instance.StartSceneLoadingWithDelay(E_SceneType.level6Bathroom, 0);
+    }
+}
