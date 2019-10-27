e4ca3ec509d218e591ebf50cb14c683a6006dbaf
yooniju <54897287+yooniju@users.noreply.github.com>
2019-10-28 04:30:45 +0900

    191028 사운드 수정 및 배치 프랍배치 플리커싱크

---
+++ DREAM205/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    AudioSource FlickerSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlickerSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+  
+    void Playsound()
+    {
+        FlickerSound.Play();
+    }
+}
