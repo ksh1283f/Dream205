43a133b8795ea5e83283e965dca32f64d986b682
yooniju <54897287+yooniju@users.noreply.github.com>
2019-11-27 01:49:30 +0900

    191127
    1. 엔딩->오프닝

---
+++ DREAM205/Assets/Scripts/Refectoring/GameManager_Ending.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Ending : Singletone<GameManager_Ending>
+{
+    private void Start()
+    {
+        SoundManager.Instance.OnSoundPlayEnd += LoadNextScene;
+        StartCoroutine(SoundManager.Instance.IntroSoundPlay());
+    }
+
+    void LoadNextScene()
+    {
+        SceneLoadingManager.Instance.StartSceneLoadingWithDelay(E_SceneType.levelOpening, 0);
+    }
+}
